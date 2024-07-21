4f11db051292cf175cd9469fa94ba4a26f2ab700
Francisco Born <fco.borndt@gmail.com>
2024-07-21 19:54:41 -0400

    release 2: adding more items and improving cart usability

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,22 +2,26 @@ import React from 'react'
 import logo_mini_mage from '../assets/img/logo-mini-mage.png'; 
 import './NavBar.css';
 import CartWidget from './CartWidget';
+import { Link } from 'react-router-dom';
 
 
-function NavBar() {
+function NavBar({cartItems}) {
     return (
         <nav>
             <div id="nav-left">
+                <Link to="/.">
                 <img src={logo_mini_mage} alt="Page logo, showing a half upper body Mini Mage with the phrase 'Mini Mage'." className="img-mini-mage"/>
+                </Link>
                 <ul id="nav-options">
-                    <li>News</li>
-                    <li>Game Guides</li>
-                    <li>Forum</li>
-                    <li>Support</li>
+                    <li><Link to="/news"> News </Link></li>
+                    <li><Link to="/game-guides"> Game Guides </Link></li>
+                    <li><Link to="/forum"> Forum </Link></li>
+                    <li><Link to="/support"> Support </Link></li>
+                    <li><Link to="/shop"> Shop </Link></li>
                 </ul>
             </div>
             <div id="nav-right">
-                <CartWidget />
+                <Link to="/shop-cart"> <CartWidget cartItems={cartItems}/> </Link>
                 <button type="button" className="btn-play-now">
                     Play Now!
                 </button>
--- src/components/ShopItemCard.jsx
+++ src/components/ShopItemCard.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import './ShopItemCard.css'
+import { Link } from 'react-router-dom';
 
-function ShopItemCard(props) {
-    const item = props.item
+function ShopItemCard({item, keyItem, addToCart}) {
     return (
-        <div className="shop-item" key={props.keyItem}>
-            <img src={`${import.meta.env.BASE_URL}img/shop-items/${item.image_src}`} />
-            <h2>{item.name}</h2>
+        <div className="shop-item" key={keyItem}>
+            <Link to={`/shop/${item.idShopItem}`}>
+                <img className="shopItemImage" src={`${import.meta.env.BASE_URL}img/shop-items/${item.image_src}`} />
+                <h2>{item.name}</h2>
+            </Link>
             <div>
-                <button className="shop-item-button"> ${item.cost} </button>
-                <button className="shop-item-button-bundle">10x ${item.cost * 9} </button>
+                <button className="shop-item-button" onClick={() => addToCart(item)}> ${item.cost} </button>
+                {item.bundle && item.bundle.length !== 0 &&
+                (<button className="shop-item-button-bundle" onClick={() => addToCart(item, true)}> ${item.bundle[0]}x ${item.cost * item.bundle[0] * item.bundle[1]} </button>)}
             </div>
+            
         </div>
     )
 };
